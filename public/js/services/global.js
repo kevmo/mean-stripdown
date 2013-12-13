e1aef0a6f7b92d74b438d56f1841a1c6af57dcfe
kevmo <k.james.moore@gmail.com>
2013-12-12 19:10:49 -0700

    added 'global' factory - checks whether window.user exists

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,4 +1,19 @@
+//return an object which you can use to ID the user,
+// logged in status
+//login/logout is a synchronous
+
+//encapsulate it such that the application doesn't directly access it
+
 window.angular.module('ngff.services.global', [])
   .factory('Global', function() {
-
+    var current_user = window.user;
+    debugger;
+    return {
+      currentUser: function (){
+        return current_user;
+      },
+      isSignedIn: function(){
+        return !!current_user;
+      }
+    };
   });
